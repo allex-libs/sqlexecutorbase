c846821d0f7146a015d4924ae93a2cdb67511a67
Andrija Petrovic <andrija.hers@gmail.com>
2023-01-25 13:21:51 +0100

    Minor change in recordset qing type + version bump

--- queueing/types/recordsetcreator.js
+++ queueing/types/recordsetcreator.js
@@ -31,7 +31,7 @@ function createRecordset (execlib, mylib) {
 
   function validator () {
     this.recordsetcount=this.recordsetcount || 1;
-    this.rowsaffectedcount=this.recordsetcount;
+    this.rowsaffectedcount= lib.isNumber(this.rowsaffectedcount) ? this.rowsaffectedcount : this.recordsetcount;
     if (this.sentence && !lib.isString(this.sentence) && lib.has(this.sentence, ['template', 'replacements'])) {
       this.sentence = mylib.sqlsentencing.processTemplate(this.sentence.template, this.sentence.replacements)
     }
