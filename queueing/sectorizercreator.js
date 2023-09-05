4dea069243dc4ad39f8edff96eddb922cf25e07a
Andrija Petrovic <andrija.hers@gmail.com>
2023-09-05 07:34:32 +0200

    queue sectorization just got more serious, FuncSector introduced

--- queueing/sectorizercreator.js
+++ queueing/sectorizercreator.js
@@ -9,21 +9,29 @@ function createSectorizer (lib, queue, queueTxned) {
     this.sector = null;
     this.sectorizer = null;
   };
-  QRunnerJobCore.prototype.run = function () {
+  QRunnerJobCore.prototype.start = function () {
     if (this.sector.error) {
       throw this.sector.error;
     }
+    if (this.sector.func != lib.dummyFunc && this.sector.items.length==0) {
+      var a = 5;
+    }
+    return this.sector.func(this.sectorizer.lastResult);
+  };
+  QRunnerJobCore.prototype.run = function () {
     if (!(lib.isArray(this.sector.items) && this.sector.items.length>0)) {
       return;
     }
-    return this.sector.func.call(this.sectorizer.executor, this.sector.items);
+    return this.sector.qHandler.call(this.sectorizer.executor, this.sector.items);
   };
   QRunnerJobCore.prototype.postRun = function (res) {
+    this.sectorizer.lastResult = res;
     if (lib.isArray(res)) {
       Array.prototype.push.apply(this.sectorizer.results, res);
     }
   };
   QRunnerJobCore.prototype.steps = [
+    'start',
     'run',
     'postRun'
   ];
@@ -53,36 +61,77 @@ function createSectorizer (lib, queue, queueTxned) {
   function isTxnEnd () {
     return this == 'COMMIT TRANSACTION';
   }
+  function isFunction() {
+    return lib.isFunction(this);
+  }
   function shouldGoToSector () {
-    return !lib.isString(this);
+    return !(lib.isString(this)||lib.isFunction(this));
   }
 
-  function Sector (qitem) {
-    this.func = isTxnBegin.call(qitem) ? queueTxned : queue;
+  function SectorBase (qitem) {
     this.items = [];
     this.done = false;
+    this.func = lib.dummyFunc;
     if (shouldGoToSector.call(qitem)) {
       this.items.push(qitem);
     }
   }
-  Sector.prototype.destroy = function () {
+  SectorBase.prototype.destroy = function () {
+    this.func = null;
     this.done = null;
     this.items = null;
-    this.func = null;
   };
-  Sector.prototype.maybeAdd = function (qitem) {
-    if (this.isTxned()) {
-      if (isTxnBegin.call(qitem)) {
-        throw new lib.Error('NESTED_TXNS_NOT_SUPPORTED');
-      }
-      if (isTxnEnd.call(qitem)) {
-        this.done = true;
-        return true;
-      }
-      if (shouldGoToSector.call(qitem)) {
-        this.items.push(qitem);
-        return true;
-      }
+  SectorBase.prototype.maybeAdd = function (qitem) {
+    if (shouldGoToSector.call(qitem)) {
+      this.items.push(qitem);
+      return true;
+    }
+    return false;
+  };
+  SectorBase.prototype.qHandler = queue;
+
+  function PlainSector (qitem) {
+    SectorBase.call(this, qitem);
+  }
+  lib.inherit(PlainSector, SectorBase);
+  PlainSector.prototype.maybeAdd = function (qitem) {
+    if (isTxnBegin.call(qitem) || isFunction.call(qitem)) {
+      this.done = true;
+      return false;
+    }
+    if (isTxnEnd.call(qitem)) {
+      throw new lib.Error('COMMIT_TXN_WITHOUT_MATCHING_BEGIN', 'Cannot have a COMMIT TRANSACTION without a matching BEGIN TRANSACTION');
+    }
+    return SectorBase.prototype.maybeAdd.call(this, qitem);
+  };
+
+  function TxnSector (qitem) {
+    SectorBase.call(this, qitem);
+  }
+  lib.inherit(TxnSector, SectorBase);
+  TxnSector.prototype.maybeAdd = function (qitem) {
+    if (isTxnBegin.call(qitem)) {
+      throw new lib.Error('NESTED_TXNS_NOT_SUPPORTED', 'Nested Transactions not supported');
+    }
+    if (isFunction.call(qitem)) {
+      throw new lib.Error('FUNCTION_IN_TXN_NOT_SUPPORTED', 'Function within a Transaction not supported');
+    }
+    if (isTxnEnd.call(qitem)) {
+      this.done = true;
+      return true;
+    }
+    return SectorBase.prototype.maybeAdd.call(this, qitem);
+  };
+  TxnSector.prototype.qHandler = queueTxned;
+
+  function FuncSector (qitem) {
+    PlainSector.call(this, qitem);
+    this.func = qitem;
+  }
+  lib.inherit(FuncSector, SectorBase);
+  FuncSector.prototype.maybeAdd = function (qitem) {
+    if (isFunction.call(qitem)) {
+      this.done = true;
       return false;
     }
     if (isTxnBegin.call(qitem)) {
@@ -90,17 +139,24 @@ function createSectorizer (lib, queue, queueTxned) {
       return false;
     }
     if (isTxnEnd.call(qitem)) {
-      throw new lib.Error('COMMIT_TXN_WITHOUT_MATCHING_BEGIN');
+      throw new lib.Error('COMMIT_TXN_IN_FUNCTION_SECTOR', 'COMMIT TRANSACTION cannot appear in a Function sector');
     }
-    if (shouldGoToSector.call(qitem)) {
-      this.items.push(qitem);
-      return true;
-    }
-    return false;
-  }
-  Sector.prototype.isTxned = function () {
-    return this.func == queueTxned;
+    return SectorBase.prototype.maybeAdd.call(this, qitem);
   };
+
+
+  function SectorFactory (qitem) {
+    if (isTxnBegin.call(qitem)) {
+      return new TxnSector(qitem);
+    }
+    if (lib.isFunction(qitem)) {
+      return new FuncSector(qitem);
+    }
+    if (isTxnEnd(qitem)) {
+      throw new lib.Error('CANNOT_PRODUCE_SECTOR', 'A sector cannot be started with '+qitem);
+    }
+    return new PlainSector(qitem);
+  }
 
   function QueueSectorizer (executor, queueobjarry) {
     this.executor = executor;
@@ -109,12 +165,14 @@ function createSectorizer (lib, queue, queueTxned) {
     this.txned = false;
     this.sectors = [];
     this.sector = null;
+    this.lastResult = null;
     this.results = [];
     this.error = null;
   }
   QueueSectorizer.prototype.destroy = function () {
     this.error = null;
     this.results = null;
+    this.lastResult = null;
     this.sector = null;
     if (this.sectors) {
       lib.arryDestroyAll(this.sectors);
@@ -139,19 +197,19 @@ function createSectorizer (lib, queue, queueTxned) {
   QueueSectorizer.prototype.itemizer = function (qitem) {
     var taken;
     if (!this.sector) {
-      this.sector = new Sector(qitem);
+      this.sector = SectorFactory(qitem);
       return;
     }
     taken = this.sector.maybeAdd(qitem);
     if (this.sector.done) {
       this.pushSector();
       if (!taken) {
-        this.sector = new Sector(qitem);
+        this.sector = SectorFactory(qitem);
         return;
       }
     }
     if (!taken) {
-      throw new lib.Error('WHY_ITEM_NOT_TAKEN?');
+      throw new lib.JSONizingError('WHY_ITEM_NOT_TAKEN?', qitem, 'Was not taken');
     }
   };
   QueueSectorizer.prototype.jobizer = function (sector) {
